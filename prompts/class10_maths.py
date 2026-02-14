2cad0d3b7f7414a978d2bb867583d7a3e29bb3ed
Skolist <tech@skolist.com>
2026-02-14 16:10:49 +0530

    feat(prompts): add CBSE Maths prompt for structured concept extraction

--- prompts/class10_maths.py
+++ prompts/class10_maths.py
@@ -2,17 +2,7 @@
 This contains the prompt for decomposing a chapter of ncert class 10 maths into concepts.
 """
 
-# NCERT_CLASS_10_Maths = f'''
-#     The chapter's pdf is already structures in a format of topics.
-#     You can directly see there are multiple topics in this chapter's pdf as headers.
-#     You need to decompose each topic into concepts.
-#     Don't apply your own knowledge to decompose the topic into concepts.
-#     Only use the information given in the chapter's pdf to decompose the topic into concepts.
-#     There must be some paragraphs, diagrams in a topic. Use them to define concepts.
-#     Concepts are smallest unit of knowledge, in this system.
-#     You should not miss any concept.
-#     Follow the given schema for outputting.
-#     '''
+
 
 
 # NCERT_CLASS_6_SCIENCE = f'''
